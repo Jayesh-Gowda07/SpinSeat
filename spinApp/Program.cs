49e6595b0422d3d95bfa86793750880ec26d9618
Jayesh-Gowda07 <jaygo@eg.dk>
2025-03-04 17:17:36 +0530

    2

--- spinApp/Program.cs
+++ spinApp/Program.cs
@@ -15,11 +15,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 // Program.cs
-builder.WebHost.UseUrls("http://*:10000");
+
 
 
 builder.Services.AddDbContext<AppDbContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("spinAppDefaultConnection")));
+// Program.cs
+builder.WebHost.UseUrls("http://*:8080");
 
 var app = builder.Build();
 
